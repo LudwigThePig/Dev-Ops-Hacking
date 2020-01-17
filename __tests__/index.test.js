1ff4106a572131ef4e6f87330b8d5f6844a984c9
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-17 12:22:24 -0700

    investigation: add failing test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,3 +4,6 @@ import func from '../src/index.js';
 it('should return the truth', () => {
   expect(func()).toBe(true);
 })
+it('should return the truth, but it won\'t...', () => {
+  expect(func()).toBe(false);
+})
