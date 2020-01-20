26f1692130e9b54dce5eeb3509dfdd59c3d5a914
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-20 16:08:08 -0700

    fix: fixed the failing test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,6 +4,3 @@ import func from '../src/index.js';
 it('should return the truth', () => {
   expect(func()).toBe(true);
 })
-it('should return the truth, but it won\'t...', () => {
-  expect(func()).toBe(false);
-})
