30df09200eb0926abba7ceb18fabe8062770f740
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-21 13:37:30 -0700

    feat: write a shell script to setup the staging server

--- .gitlab/deploy-staging.sh
+++ .gitlab/deploy-staging.sh
@@ -1,1 +1,8 @@
+# !/bin/bash
+# Get servers list:
+set —f
+# Variables from GitLab server:
+# Note: They can’t have spaces!!
+ssh $STAGING_SERVER "cd ./staging/devops-investigation && git stash && git checkout $CI_BUILD_REF_NAME && git stash && git pull && sudo npm install && sudo npm run prod:start"
+
 echo "Alexa, deploy to staging!"
