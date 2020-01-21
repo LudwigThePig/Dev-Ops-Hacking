4ddb23bd9ef8a155cb9e44a92cfdc47194308c49
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-21 14:34:23 -0700

    chore: remove the echos and add a git pull

--- .gitlab/deploy-staging.sh
+++ .gitlab/deploy-staging.sh
@@ -4,9 +4,6 @@ set —f
 # Variables from GitLab server:
 # Note: They can’t have spaces!!
 
-echo $SSH_PRIVATE_KEY
-echo $SSH_PRIVATE_KEY
-echo $STAGING_SERVER
-ssh $STAGING_SERVER "echo 'SSHEDINT' && cd ./staging/devops-investigation && echo $(pwd) && echo 'ALEXA LAUNCH IT!' && git stash && git checkout $CI_BUILD_REF_NAME && git stash && git pull && sudo npm install && sudo npm run prod:start"
+ssh $STAGING_SERVER "cd ./staging/devops-investigation && git stash && git pull && git checkout $CI_BUILD_REF_NAME && git stash && git pull && sudo npm install && sudo npm run prod:start"
 
 echo "Alexa, deploy to staging!"
