d0816ef876071e3a91f54e4a5b43ff24a57f6585
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-21 13:57:20 -0700

    dev

--- .gitlab/deploy-staging.sh
+++ .gitlab/deploy-staging.sh
@@ -3,6 +3,7 @@
 set —f
 # Variables from GitLab server:
 # Note: They can’t have spaces!!
+echo $STAGING_SERVER
 ssh $STAGING_SERVER "cd ./staging/devops-investigation && echo $(pwd) && echo "ALEXA LAUNCH IT!" && git stash && git checkout $CI_BUILD_REF_NAME && git stash && git pull && sudo npm install && sudo npm run prod:start"
 
 echo "Alexa, deploy to staging!"
