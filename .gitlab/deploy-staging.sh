d5ae0adf5cc5eb90fca0cd431df9000d7b9424fc
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-21 16:19:20 -0700

    chore: add some helpful echos

--- .gitlab/deploy-staging.sh
+++ .gitlab/deploy-staging.sh
@@ -4,6 +4,6 @@ set —f
 # Variables from GitLab server:
 # Note: They can’t have spaces!!
 
-ssh $STAGING_SERVER "cd ./staging/devops-investigation && git stash && echo 'pulling' && git pull && echo 'checking out' && git checkout $CI_BUILD_REF_NAME && git stash && echo 'pulling again' && git pull && npm install && npm run prod:start"
+ssh $STAGING_SERVER "cd ./staging/devops-investigation && git stash && echo 'pulling' && git pull && echo 'checking out' && git checkout $CI_BUILD_REF_NAME && git stash && echo 'pulling again' && git pull && echo 'Installing!' && npm install &&  echo 'starting the server!' && npm run prod:start"
 
 echo "Alexa, deploy to staging!"
