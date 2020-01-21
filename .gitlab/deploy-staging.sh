0034cea8b1580d1d9b0f81567418ce0be63ded82
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-21 14:12:36 -0700

    devs: oops, how do you spell echo?

--- .gitlab/deploy-staging.sh
+++ .gitlab/deploy-staging.sh
@@ -4,7 +4,7 @@ set —f
 # Variables from GitLab server:
 # Note: They can’t have spaces!!
 
-echot $SSH_PRIVATE_KEY
+echo $SSH_PRIVATE_KEY
 echo $STAGING_SERVER
 ssh $STAGING_SERVER "echo 'SSHEDINT' && cd ./staging/devops-investigation && echo $(pwd) && echo 'ALEXA LAUNCH IT!' && git stash && git checkout $CI_BUILD_REF_NAME && git stash && git pull && sudo npm install && sudo npm run prod:start"
 
