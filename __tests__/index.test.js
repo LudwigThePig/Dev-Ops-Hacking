bfb0d46ecc7a62b73fcea9d97848fa822cb73c14
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-20 17:21:20 -0700

    tests: add tests specific to the settings

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,53 @@
-import func from '../src/index.js';
+import{ 
+  mittenRockMaterial, 
+  rotation,
+  lights 
+} from '../src/settings.js';
+
+describe('Mitten Rock Material', () => {
+  it('it should not be decaf ☕️ 😴', () => {
+    expect(mittenRockMaterial.color).not.toBe(0xDECAFF);
+  });
+
+  it('it should be shiny ', () => {
+    expect(mittenRockMaterial.shininess).toBeGreaterThan(500);
+  });
+
+  it('it have flat shading', () => {
+    expect(mittenRockMaterial.flatShading);
+  });
+});
 
 
-it('should return the truth', () => {
-  expect(func()).toBe(true);
-})
+describe('Rotation', () => {
+  it('should not rotate on the x axis', () => {
+    expect(rotation.x).toBe(0);
+  });
+
+  it('should rotate faster than the earth on the y axis 🌍 🐢', () => {
+    const yearsPerFrame = 365 / 60 / 60 / 24 / 365;
+    expect(rotation.y).toBeGreaterThan(yearsPerFrame);
+  });
+
+  it('should rotate slower than the electron of a hydrogen molecule ⚛️', () => {
+    const revolutionsPerFrame = 10e15 / 60;
+    expect(rotation.y).toBeLessThan(revolutionsPerFrame);
+  });
+});
+
+describe('Lights', () => {
+
+  it('should have a neutral light', () => {
+    expect(lights.ambient[0]).toBeGreaterThan(0xDEDEDE);
+    expect(lights.point[0]).toBeGreaterThan(0xDEDEDE);
+  });
+
+  it('generally, should not be too bright 🎚', () => {
+    expect(lights.ambient[1]).toBeLessThan(1);
+
+  });
+
+  it('in spots, should be bright ☀️', () => {
+    expect(lights.point[1]).toBeGreaterThan(1);
+  });
+});
