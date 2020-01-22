164eb29a56cbd72a69bde0ed4c39a88c6637e191
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-22 12:17:28 -0700

    fix: change "- silent" to "--silent"

--- .gitlab/auto-pull-request.sh
+++ .gitlab/auto-pull-request.sh
@@ -12,7 +12,7 @@ fi
 # Extract the host where the server is running, and add the URL to the APIs
 [[ $CI_PROJECT_URL =~ ^https?://[^/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
 # Look which is the default branch
-TARGET_BRANCH=`curl — silent "${HOST}${CI_PROJECT_ID}" — header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" | jq — raw-output ‘.default_branch’`;
+TARGET_BRANCH=`curl —-silent "${HOST}${CI_PROJECT_ID}" — header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" | jq — raw-output '.default_branch'`;
 # The description of our new MR, we want to remove the branch after the MR has
 # been closed
 BODY="{
@@ -27,7 +27,7 @@ BODY="{
 # one with the same source branch
 LISTMR=`curl — silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" — header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
 COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
-# No MR found, let’s create a new one
+# No MR found, let's create a new one
 if [ ${COUNTBRANCHES} -eq "0" ]; then
  curl -X POST "${HOST}${CI_PROJECT_ID}/merge_requests" \
  — header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" \
